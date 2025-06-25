0456f3b7da459e6d16b24aea85d8d28d2da1f3dd
dfleis <david.p.fleischer@gmail.com>
2025-06-25 17:31:47 -0400

    adding some utilities specific to S7 validation

---
+++ R/S7-utils.R
@@ -0,0 +1,75 @@
+#' @keywords internal
+#' @noRd
+is_S7_Class <- function(Class) {
+  # Test for class generators, adapted from S7:::is_S7_type
+  S7::S7_inherits(Class) && (typeof(Class) == typeof(S7::new_class(".")))
+}
+
+#' @keywords internal
+#' @noRd
+is_S7_instance <- function(x) {
+  # Test for class instances, adapted from S7:::is_S7_type
+  S7::S7_inherits(x) && (typeof(x) == typeof(S7::S7_object()))
+}
+
+#' @keywords internal
+#' @noRd
+obj_type <- function (x) {
+  # Taken from S7:::obj_type
+  if (identical(x, quote(expr = ))) {
+    "missing"
+  } else if (inherits(x, "S7_object")) {
+    "S7"
+  } else if (isS4(x)) {
+    "S4"
+  } else if (is.object(x)) {
+    "S3"
+  } else {
+    "base"
+  }
+}
+
+#' @keywords internal
+#' @noRd
+obj_desc <- function (x) {
+  # Taken from S7:::obj_desc
+  switch(
+    obj_type(x),
+    missing = "MISSING",
+    base = paste0("<", typeof(x), ">"),
+    S3 = paste0("S3<", paste(class(x), collapse = "/"), ">"),
+    S4 = paste0("S4<", class(x), ">"),
+    S7 = paste0("<", class(x)[[1]], ">")
+  )
+}
+
+#' @keywords internal
+#' @noRd
+classname <- S7::new_generic(".classname", "x")
+
+#' @keywords internal
+#' @noRd
+S7::method(classname, S7::S7_object) <- function(x, ...) {
+  if (is_S7_instance(x)) {
+    x <- S7::S7_class(x)
+  }
+  pkg <- x@package # S7::prop(x, "package")
+  nm <- x@name     # S7::prop(x, "name")
+  sprintf("%s%s", toString(sprintf("%s::", pkg)), nm)
+}
+
+#' @keywords internal
+#' @noRd
+assert_S7_generator_name <- function(Class, arg = deparse(substitute(Class))) {
+  # Gets the class name for instances of an S7 class generator `Class`
+  # Adapted from S7:::S7_class_name, S7::S7_inherits, S7::check_is_S7
+  if (!isTRUE(is_S7_Class(Class))) {
+    msg <- sprintf(
+      "Object %s must be an <S7_class>, not %s", arg,
+      if (.is_S7_instance(Class)) "an <S7_object>"
+      else paste("a", obj_desc(Class))
+    )
+    stop(msg, call. = FALSE)
+  }
+  classname(Class)
+}
--- R/print.R
+++ R/print.R
@@ -1,40 +1,34 @@
 #' @include classes.R
+#' @include S7-utils.R
 NULL
 
 #----- Print utilities
 #' @keywords internal
 #' @noRd
-.cap_str <- function(x, give.attr = FALSE, ...) {
+cap_str <- function(x, give.attr = FALSE, ...) {
   capture.output(str(x, give.attr = give.attr, ...))
 }
 
 #' @keywords internal
 #' @noRd
-.format_classname <- S7::new_generic("format_classname", "x")
-
-#' @keywords internal
-#' @noRd
-S7::method(.format_classname, S7::S7_object) <- function(x, ...) {
-  class_obj <- S7::S7_class(x)
-  pkg <- class_obj@package # S7::prop(class_obj, "package")
-  nm <- class_obj@name     # S7::prop(class_obj, "name")
-  sprintf("<%s%s>", toString(sprintf("%s::", pkg)), nm)
+fmt_classname <- function(x, ...) {
+  sprintf("<%s>", classname(x))
 }
 
 #----- Print methods
 #' @export
 S7::method(print, rfg_basis_params) <- function(x, detailed = TRUE, ...) {
   if (!isTRUE(detailed)) {
-    class_info <- .format_classname(x)
+    class_info <- fmt_classname(x)
     
     str_a <- sprintf("@a %6.3f", x@a)
-    str_phi <- sprintf("@phi %s", .cap_str(x@phi, give.head = F, vec.len = 2)[1])
+    str_phi <- sprintf("@phi %s", cap_str(x@phi, give.head = F, vec.len = 2)[1])
     
-    str_mu <- sprintf("@mu%s", sub(" NULL ...", "", .cap_str(x@mu, vec.len = 0)[1]))
+    str_mu <- sprintf("@mu%s", sub(" NULL ...", "", cap_str(x@mu, vec.len = 0)[1]))
     if (length(x@mu) == 1L) {
       str_mu <- paste(str_mu, "[1:1]")
     }
-    str_V <- sprintf("@V%s", sub(" NULL ...", "", .cap_str(x@V, vec.len = 0)[1]))
+    str_V <- sprintf("@V%s", sub(" NULL ...", "", cap_str(x@V, vec.len = 0)[1]))
     
     cat(class_info, paste(str_a, str_mu, str_V, str_phi, sep = ", "), "\n")
     return(invisible(x))
@@ -45,7 +39,7 @@ S7::method(print, rfg_basis_params) <- function(x, detailed = TRUE, ...) {
 
 #' @export
 S7::method(print, rfg_params) <- function(x, detailed = FALSE, ...) {
-  cat(sprintf("%s\n", .format_classname(x)))
+  cat(classname(x), "\n")
   cat(" @ p    :")
   cat(capture.output(str(x@p, give.attr = FALSE))[1], "\n")
   cat(" @ bases:")
@@ -71,7 +65,7 @@ S7::method(print, rfg_function) <- function(x, detailed = FALSE, ...) {
   # p_dim <- if (q_dim > 0L) param_list[[1]]@p else 0L
   # 
   # cat(sprintf("Generated RFG map from R^%i to R^%i\n", p_dim, q_dim))
-  # cat(sprintf("%s\n", .format_classname(x)))
+  # cat(fmt_classname(x), "\n")
   # 
   # if (q_dim > 0L) {
   #   cat(sprintf(" @ params:%s of %i\n", class(param_list)[1], q_dim))
